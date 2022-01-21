dc6339088e5acd174682393aadabb798f577c4ca
Nathay Mesquita <nnathalygm@gmail.com>
2022-01-21 01:53:03 -0300

    feat(): Adicionando Enum Project Status

---
+++ src/Domain/Enums/ProjectStatusEnum.cs
@@ -0,0 +1,11 @@
+namespace PI.Integration.TCC.Domain.Enums
+{
+    public enum ProjectStatusEnum
+    {
+        Created = 0,
+        InProgress = 1,
+        Suspended = 2,
+        Cancelled = 3,
+        Finished = 4
+    }
+}
