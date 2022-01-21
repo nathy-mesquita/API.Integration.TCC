5f89ac3146fa7390dbdf1c612b2f7361ff9dc764
Nathay Mesquita <nnathalygm@gmail.com>
2022-01-21 02:23:32 -0300

    feat(): Adicionando ITeacherRepository

---
+++ src/Domain/Repositories/ITeacherRepository.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using API.Integration.TCC.Domain.Entities;
+
+namespace API.Integration.TCC.Domain.Repositories
+{
+    public interface ITeacherRepository
+    {
+        Task<List<Teacher>> GetAllAsync();
+        Task<Teacher> GetByIdAsync(int id);
+        Task<Teacher> GetUserByEmailAndPasswordAsync(string email, string passwordHash);
+        Task  AddAsync(Teacher user);
+    }
+}
