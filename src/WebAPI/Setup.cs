6f2c17c1233371f9f587c460b8f510920bb7b561
Nathay Mesquita <nnathalygm@gmail.com>
2022-01-22 23:55:41 -0300

    feat(): Setup

---
+++ src/WebAPI/Setup.cs
@@ -0,0 +1,94 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+using API.Integration.TCC.WebAP.Filters;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Integration.TCC.WebAP
+{
+    public static class Setup
+    {
+        public static void AddWebApi(this IServiceCollection services)
+        {
+            services.AddControllers(options => options.Filters.Add(typeof(ValidatorFilter)))
+                    .AddJsonOptions(options =>
+                    {
+                        options.JsonSerializerOptions.IgnoreNullValues = true;
+                    });
+                    //.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateUserCommandValidator>());
+            
+            services.AddHealthChecks();
+        }
+
+        public static void UseWebApi(this IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
+            });
+        }
+
+        public static void AddSwaggerApiDoc(this IServiceCollection services)
+        {
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API de Integração para Trabalho de Conclusão de Curso", Version = "v1" });
+
+                var currentAssembly = Assembly.GetExecutingAssembly();
+                var xmlDocs = currentAssembly.GetReferencedAssemblies()
+                    .Union(new AssemblyName[] { currentAssembly.GetName() })
+                    .Select(a => Path.Combine(Path.GetDirectoryName(currentAssembly.Location)!, $"{a.Name}.xml"))
+                    .Where(f => File.Exists(f)).ToArray();
+                Array.ForEach(xmlDocs, (d) => c.IncludeXmlComments(d));
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header usando o esquema Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+        }
+
+        public static void UseSwaggerApiDoc(this IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.DocumentTitle = "API Integration TCC | Nathaly & Misael Swagger";
+                c.InjectStylesheet("/swagger-ui/custom.css");
+                c.InjectJavascript("/swagger-ui/custom.js");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Version 1");
+            });
+        }
+    }
+}
