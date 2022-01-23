7f12559b6d0f4a5ab3265e0f1dde8a8b1fa44488
Nathay Mesquita <nnathalygm@gmail.com>
2022-01-22 23:53:17 -0300

    feat(): Roles

---
+++ src/WebAPI/Roles.cs
@@ -0,0 +1,9 @@
+namespace API.Integration.TCC.WebAP
+{
+    public static class Roles
+    {
+        public const string Administrador = "IntegrationTCC_Admin";
+        public const string Student = "IntegrationTCC_Student";
+        public const string Teacher = "IntegrationTCC_Teacher";
+    }
+}
