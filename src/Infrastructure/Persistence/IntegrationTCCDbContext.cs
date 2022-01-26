05a38822fb4b2db36dffbc26e82f4aff09006e97
Nathay Mesquita <nnathalygm@gmail.com>
2022-01-26 00:56:01 -0300

    feat(): IntegrationTCCDbContext

--- src/Infrastructure/Persistence/IntegrationTCCDbContext.cs
+++ src/Infrastructure/Persistence/IntegrationTCCDbContext.cs
@@ -1,7 +1,23 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using API.Integration.TCC.Infrastructure.Persistence.Repositories;
+
 namespace API.Integration.TCC.Infrastructure.Persistence
 {
-    public class IntegrationTCCDbContext
+    public class IntegrationTCCDbContext : DbContext
     {
-        
+        public IntegrationTCCDbContext(DbContextOptions<IntegrationTCCDbContext> options): base(options)
+        {
+        }
+        public DbSet<ProjectTCCRepository>? ProjectsTCC { get; set; }
+        public DbSet<StudentRepository>? Students { get; set; }
+        public DbSet<TeacherRepository>? Teachers { get; set; }
+        public DbSet<ProjectTCCCommentsRepository>? ProjectTCCComments { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {            
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
