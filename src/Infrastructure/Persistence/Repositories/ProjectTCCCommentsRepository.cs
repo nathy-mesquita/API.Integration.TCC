49fb35596d33f1976395b5bc566e37f218f80c6b
Nathay Mesquita <nnathalygm@gmail.com>
2022-01-26 01:08:56 -0300

    fix(): Inclusão do dbContext

--- src/Infrastructure/Persistence/Repositories/ProjectTCCCommentsRepository.cs
+++ src/Infrastructure/Persistence/Repositories/ProjectTCCCommentsRepository.cs
@@ -7,6 +7,10 @@ namespace API.Integration.TCC.Infrastructure.Persistence.Repositories
 {
     public class ProjectTCCCommentsRepository : IProjectTCCCommentsRepository
     {
+        private readonly IntegrationTCCDbContext _dbContext;
+
+        public ProjectTCCCommentsRepository(IntegrationTCCDbContext dbContext) => _dbContext = dbContext;
+
         public Task AddAsync(ProjectTCCComments comments)
         {
             throw new System.NotImplementedException();
