af5ed9196e92ce9483a3455654a521c95310e0ab
Nathay Mesquita <nnathalygm@gmail.com>
2022-01-26 00:55:33 -0300

    fix(): adicionando Repository

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -2,6 +2,8 @@ using Microsoft.Extensions.Configuration;
 using API.Integration.TCC.Domain.Services;
 using API.Integration.TCC.Infrastructure.Auth;
 using Microsoft.Extensions.DependencyInjection;
+using API.Integration.TCC.Domain.Repositories;
+using API.Integration.TCC.Infrastructure.Persistence.Repositories;
 
 namespace API.Integration.TCC.Infrastructure
 {
@@ -9,10 +11,10 @@ namespace API.Integration.TCC.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            // services.AddScoped<IProjectRepository, ProjectRepository>();
-            // services.AddScoped<ISkillRepository, SkillRepository>();
-            // services.AddScoped<IUserRepository, UserRepository>();
-            // services.AddScoped<IUserSkillRepository, UserSkillRepository>();
+            services.AddScoped<IProjectTCCCommentsRepository, ProjectTCCCommentsRepository>();
+            services.AddScoped<IProjectTCCRepository, ProjectTCCRepository>();
+            services.AddScoped<IStudentRepository, StudentRepository>();
+            services.AddScoped<ITeacherRepository, TeacherRepository>();
 
             services.AddScoped<IAuthService, AuthService>();
             
