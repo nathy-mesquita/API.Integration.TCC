6e6dfc6ded4014bfa8249f288c9cb14a549afd5b
Nathaly Mesquita <nathaly.mesquita@xpi.com.br>
2022-02-03 00:29:44 -0300

    feat(): DependecyInjection

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace API.Integration.TCC.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var assemblies = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(assemblies);
+            //services.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateStudentValidator>());
+            //services.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateTeacherValidator>());
+
+            return services;
+        }
+    }
+}
